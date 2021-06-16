7a5d99cd49b673ba903fda44ffc710f33a5da6b5
Jackson Asprilla Blandon <yayokalonji@Jacksons-MacBook-Pro.local>
2021-06-16 11:09:18 -0500

    Fix: Add Ngrx entitys

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
