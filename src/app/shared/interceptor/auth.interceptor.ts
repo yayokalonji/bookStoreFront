55c7c79928c56c2b40b863124b2e4f98a6a83a9f
Jackson Asprilla Blandon <yayokalonji@Jacksons-MacBook-Pro.local>
2021-07-13 14:22:57 -0500

    Fix: Refactor interceptor

--- src/app/shared/interceptor/auth.interceptor.ts
+++ src/app/shared/interceptor/auth.interceptor.ts
@@ -5,54 +5,59 @@ import {
   HttpEvent,
   HttpInterceptor,
   HttpErrorResponse,
-  HttpHeaders
+  HttpHeaders,
 } from '@angular/common/http';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { catchError, switchMap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment.prod';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private user: { userName: string; password: string } = {
+    userName: environment.user,
+    password: environment.key,
+  };
 
-  private isRefreshing = false;
-  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  constructor(public authService: AuthService) {}
 
-  private user: { userName: string, password: string } = { userName: environment.user, password: environment.key};
-
-  constructor( public authService: AuthService) {}
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.authService.getJwtToken()){
-      request = this.addToken(request, this.authService.getJwtToken());
-    }
-    return next.handle(request).pipe(catchError(error => {
-      if (error instanceof HttpErrorResponse && (error.status === 401 || error.status === 403 || error.status === 500)){
-        return this.handle401Error(request, next);
-      }else {
-        console.log(error);
-        return throwError(error);
-      }
-    }));
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (
+          error instanceof HttpErrorResponse &&
+          (error.status === 401 || error.status === 403)
+        ) {
+          return this.handle401Error(request, next);
+        } else {
+          return throwError(error);
+        }
+      })
+    );
   }
 
-  private addToken(request: HttpRequest<any>, token: any | null): HttpRequest<any> {
+  private addToken(
+    request: HttpRequest<any>,
+    token: any | null
+  ): HttpRequest<any> {
     return request.clone({
-      headers : new HttpHeaders({
-        Authorization: `${token}`
-      })
+      headers: new HttpHeaders({
+        Authorization: `${token}`,
+      }),
     });
   }
 
-  private handle401Error(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.isRefreshing = true;
-    this.refreshTokenSubject.next(null);
+  private handle401Error(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     return this.authService.login(this.user).pipe(
-    switchMap((token: any) => {
-      this.isRefreshing = false;
-      this.refreshTokenSubject.next(token.token);
-      return next.handle(this.addToken(request, token.token));
-    }));
+      switchMap((token: any) => {
+        return next.handle(this.addToken(request, token.token));
+      })
+    );
   }
 }
-
